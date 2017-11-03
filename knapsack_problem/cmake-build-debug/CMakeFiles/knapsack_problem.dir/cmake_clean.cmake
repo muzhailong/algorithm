32f762fada1d101164e19538a417a4373ab1da5a
muzhailong <1993414600@qq.com>
2017-11-03 08:42:05 +0800

    init

---
+++ knapsack_problem/cmake-build-debug/CMakeFiles/knapsack_problem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/knapsack_problem.dir/main.cpp.obj"
+  "knapsack_problem.pdb"
+  "knapsack_problem.exe"
+  "knapsack_problem.exe.manifest"
+  "libknapsack_problem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/knapsack_problem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
